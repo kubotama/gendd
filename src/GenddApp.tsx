d650bc27b25d130a130fc5412d064b70e0c0ff98
kubotama <16697573+kubotama@users.noreply.github.com>
2021-02-13 16:04:04 +0900

    refactor(DateRow): 「日付データ」行をコンポーネントにする
    日付データのラベルとテキストボックスを組み合わせて、コンポーネントを作成する。
    
    re #2

--- src/GenddApp.tsx
+++ src/GenddApp.tsx
@@ -4,13 +4,19 @@ import './GenddApp.css';
 function GenddApp() {
   return (
     <div className="GenddApp">
-      <div>
-        <label htmlFor="date-input">日付データ</label>
-        <input type="text" id="date-input" placeholder="日付データ" />
-      </div>
+      <DateRow />
       <button>生成</button>
     </div>
   );
+}
+
+class DateRow extends React.Component {
+  render() {
+    return<div>
+    <label htmlFor="date-input">日付データ</label>
+    <input type="text" id="date-input" placeholder="日付データ" />
+  </div>
+  }
 }
 
 export default GenddApp;
