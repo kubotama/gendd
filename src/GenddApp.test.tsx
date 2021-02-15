d87c56b89dcf867238d19e4ac67f9d042e9fd26a
kubotama <16697573+kubotama@users.noreply.github.com>
2021-02-15 16:48:10 +0900

    test: spyOnの後処理をafterEachにまとめる
    GenddApp.generateDummyDateのspyOnの後処理をafterEachにまとめる。そのためにテストの位置を入れ替える。Prettierでフォーマットを修正する。

--- src/GenddApp.test.tsx
+++ src/GenddApp.test.tsx
@@ -1,13 +1,13 @@
 /**
  * @jest-environment jsdom
  */
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import {GenddApp, GenddButton} from './GenddApp';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { GenddApp, GenddButton } from "./GenddApp";
 
 describe("要素の存在", () => {
-  test('「生成」ボタンが存在', () => {
+  test("「生成」ボタンが存在", () => {
     // Arrange
 
     // Act
@@ -15,7 +15,7 @@ describe("要素の存在", () => {
 
     // Assert
     expect(screen.getByText("生成")).toBeInTheDocument();
-  })
+  });
 
   test("「日付データ」ラベルが存在", () => {
     // Arrange
@@ -25,7 +25,7 @@ describe("要素の存在", () => {
 
     // Assert
     expect(screen.getByLabelText("日付データ")).toBeInTheDocument();
-  })
+  });
 
   test("「日付データ」テキストボックスが存在", () => {
     // Arrange
@@ -35,11 +35,11 @@ describe("要素の存在", () => {
 
     // Assert
     expect(screen.getByPlaceholderText("日付データ")).toBeInTheDocument();
-  })
-})
+  });
+});
 
 describe("要素の初期値", () => {
-  test('「日付データ」テキストボックスの初期値は空白', () => {
+  test("「日付データ」テキストボックスの初期値は空白", () => {
     // Arrange
 
     // Act
@@ -47,11 +47,11 @@ describe("要素の初期値", () => {
 
     // Assert
     expect(screen.getByPlaceholderText("日付データ")).toHaveValue("");
-  })
-})
+  });
+});
 
-describe("ボタンをクリック", () => {
-  test('「生成」ボタンをクリックするとonClickGendd関数が一回呼び出される(GenddButton)', () => {
+describe("ボタンをクリック (spyOnなし)", () => {
+  test("「生成」ボタンをクリックするとonClickGendd関数が一回呼び出される(GenddButton)", () => {
     // Arrange
     const mockClick = jest.fn();
     render(<GenddButton onClick={mockClick} />);
@@ -62,55 +62,71 @@ describe("ボタンをクリック", () => {
 
     // Assert
     expect(mockClick).toBeCalledTimes(1);
-  })
-
-  test('「生成」ボタンをクリックするとonClickGendd関数が一回呼び出される(GenddApp)', () => {
-    // Arrange
-    render(<GenddApp />)
-    const spyClick = jest.spyOn(GenddApp.prototype, "generateDummyDate").mockImplementation(() => 1613268657038);
-
-    // Act
-    userEvent.click(screen.getByText("生成"));
-
-    // Assert
-    expect(spyClick).toHaveBeenCalledTimes(1);
-    (GenddApp.prototype.generateDummyDate as jest.Mock).mockRestore();
-  })
+  });
 
   interface TestValueExpected {
-    value: number,
-    expected: string
+    value: number;
+    expected: string;
   }
 
-  test.each `
-  value | expected
-  ${1613268657038} | ${"2021/2/14 11:10:57"}
-  ${1613273412347} | ${"2021/2/14 12:30:12"}
-  ` ('時刻値($value)を文字列($expected)に変換する', ({value, expected}: TestValueExpected) => {
-    // Arrange
-    const genddApp = new GenddApp({});
-    // Act
-    const dateString = genddApp.toLocaleString(value);
-    // Assert
-    expect(dateString).toBe(expected);
-  })
+  test.each`
+    value            | expected
+    ${1613268657038} | ${"2021/2/14 11:10:57"}
+    ${1613273412347} | ${"2021/2/14 12:30:12"}
+  `(
+    "時刻値($value)を文字列($expected)に変換する",
+    ({ value, expected }: TestValueExpected) => {
+      // Arrange
+      const genddApp = new GenddApp({});
+      // Act
+      const dateString = genddApp.toLocaleString(value);
+      // Assert
+      expect(dateString).toBe(expected);
+    }
+  );
 
-test.each `
-value | expected
-${1613268657038} | ${"2021/2/14 11:10:57"}
-${1613273412347} | ${"2021/2/14 12:30:12"}
-` ('「生成」ボタンをクリックして日時データ($expected)を生成する', ({value, expected}: TestValueExpected) => {
-  // Arrange
-  render(<GenddApp />);
-  const spy = jest.spyOn(GenddApp.prototype, "generateDummyDate").mockImplementation(() => value);
+  describe("ボタンをクリック (spyOnあり)", () => {
+    afterEach(() => {
+      (GenddApp.prototype.generateDummyDate as jest.Mock).mockRestore();
+    });
 
-  // Act
-  userEvent.click(screen.getByText("生成"));
-  const input = screen.getByPlaceholderText("日付データ") as HTMLInputElement;
+    test("「生成」ボタンをクリックするとonClickGendd関数が一回呼び出される(GenddApp)", () => {
+      // Arrange
+      render(<GenddApp />);
+      const spyGendd = jest
+        .spyOn(GenddApp.prototype, "generateDummyDate")
+        .mockImplementation(() => 1613268657038);
 
-  // Assert
-  expect(input.value).toBe(expected);
-  expect(spy).toHaveBeenCalledTimes(1);
-  (GenddApp.prototype.generateDummyDate as jest.Mock).mockRestore();
-  })
-})
+      // Act
+      userEvent.click(screen.getByText("生成"));
+
+      // Assert
+      expect(spyGendd).toHaveBeenCalledTimes(1);
+    });
+
+    test.each`
+      value            | expected
+      ${1613268657038} | ${"2021/2/14 11:10:57"}
+      ${1613273412347} | ${"2021/2/14 12:30:12"}
+    `(
+      "「生成」ボタンをクリックして日時データ($expected)を生成する",
+      ({ value, expected }: TestValueExpected) => {
+        // Arrange
+        render(<GenddApp />);
+        const spyGendd = jest
+          .spyOn(GenddApp.prototype, "generateDummyDate")
+          .mockImplementation(() => value);
+
+        // Act
+        userEvent.click(screen.getByText("生成"));
+        const input = screen.getByPlaceholderText(
+          "日付データ"
+        ) as HTMLInputElement;
+
+        // Assert
+        expect(input.value).toBe(expected);
+        expect(spyGendd).toHaveBeenCalledTimes(1);
+      }
+    );
+  });
+});
