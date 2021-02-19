bbdb3bb32efa82d74c815e7c96bf022a6d9d2092
kubotama <16697573+kubotama@users.noreply.github.com>
2021-02-19 21:42:38 +0900

    feat(GenddApp): importの指定を変更する

--- src/GenddApp.tsx
+++ src/GenddApp.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import InputLabel from "@material-ui/core/InputLabel";
+import { Button, InputLabel, TextField } from "@material-ui/core";
 import "./GenddApp.css";
 
 export default function GenddApp() {
